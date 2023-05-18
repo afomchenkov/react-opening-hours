3c5558b1d169b6012f265384e5af2c5ba11644fc
famchankou <famchankou@gmail.com>
2023-05-18 16:14:48 +0200

    feat: add hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,68 @@
+import { useEffect, useState, useMemo } from 'react';
+import { parseOpeningHours } from '../utils';
+import {
+  DayOfWeek,
+  DayOpenings,
+  OpeningHoursDto,
+} from '../types';
+
+const DEFAULT_OPTIONS = {
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+};
+
+export const dispatchRequest = async <T>(
+  url: string,
+  options: any = DEFAULT_OPTIONS
+): Promise<T> => {
+  const request = new Request(url, options);
+  const response = await fetch(request);
+  return await response.json() as T;
+};
+
+export const useFetch = <T>(url: string, options?: any) => {
+  const [response, setResponse] = useState<T | null>(null);
+  const [error, setError] = useState<Error | unknown | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    (async () => {
+      try {
+        const response = await dispatchRequest<T>(url, options);
+        setResponse(response);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { response, error, isLoading };
+}
+
+type UseOpeningHours = (data: OpeningHoursDto) => {
+  today: DayOfWeek
+  daysWithOpenings: DayOpenings[]
+}
+
+export const useOpeningHours: UseOpeningHours = (data) => {
+  const days: DayOfWeek[] = Object.values(DayOfWeek);
+  const today = days[new Date().getDay()];
+
+  const parsedOpeningHours = useMemo(() => {
+    const daysWithOpenings: DayOpenings[] = parseOpeningHours(data);
+
+    return {
+      today,
+      daysWithOpenings,
+    }
+  }, [data, today])
+
+  return parsedOpeningHours;
+}
