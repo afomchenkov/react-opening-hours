67b4e169dd1b6197fe2ddc6fd00d8132292e548c
famchankou <famchankou@gmail.com>
2023-05-18 14:25:24 +0200

    feat: add main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useFetch } from './hooks';
+import OpeningHours from './components/opening-hours/OpeningHours';
+import './App.scss';
 
-function App() {
+const App = () => {
+  const { response: schedule, isLoading } = useFetch<any>('data.json');
+
+  if (isLoading || !schedule) {
+    return null
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <OpeningHours data={schedule}/>
+    </>
   );
 }
 
